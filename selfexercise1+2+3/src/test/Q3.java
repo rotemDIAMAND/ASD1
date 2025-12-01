80375123fd172343c6f1255419686a284952b25b
rotemDIAMAND <rotemdiamond100@gmail.com>
2025-12-01 18:54:38 +0200

    start

---
+++ selfexercise1+2+3/src/test/Q3.java
@@ -0,0 +1,15 @@
+package test;
+import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Q3 {
+    public static double calc(String expression){
+        Stack<String> numbers = new Stack<>();
+        Queue<String> operators = new LinkedList<>();
+        System.out.println("Enter your exercise:");
+        String exercise = scanner.nextInt();
+
+    }
+}
